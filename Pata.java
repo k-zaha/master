07641feaab70a4f4357b9b566e5ee335f236165f
k-zaha <kzaha@aol.com>
2020-01-05 12:32:19 +0900

    Pataクラスを見直し

--- Pata.java
+++ Pata.java
@@ -1,12 +1,19 @@
 class Pata{
 
     public static void main(String[] args) {
-        System.out.println("member:" + jjc.length);
-        //int a = 0;
-        for(int a = 0; args.length > a; a++){
+        if(args.length == 0){
+            System.out.println("コマンドライン引数が入っていません(><)");
+        } else if(args.length >= 9) {
+            System.out.println("member:9");
+        } else {
+            System.out.println("member:" + args.length);
+        }
+
+        for(int a = 0; a < args.length; a++){
             int b = a + 1;
             System.out.println("[" + b + "]:" + args[a]);
-            if(a > 10){
+            if(a > 7){ //aが数字＋1の状態で通過するまでスキップし続けるため、カウントしたい数字分の、－2でカウント制限をかけられる。
+                //System.out.println(a);
                 break;
             }
         }
